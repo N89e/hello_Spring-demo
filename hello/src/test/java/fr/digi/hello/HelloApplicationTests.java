cc07a0f2cab9055038f1d34fa75e53cef744654e
N89e <nunodafonseca@live.fr>
2025-08-20 10:43:46 +0200

    premier envoi

---
+++ hello/src/test/java/fr/digi/hello/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.digi.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
